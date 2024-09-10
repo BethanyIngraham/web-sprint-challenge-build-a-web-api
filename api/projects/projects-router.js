734503e8b2330225a78a74a2f5b6d1b43d3f8351
Bethany Ingraham <BethFIngraham@gmail.com>
2024-09-10 07:27:04 -0400

    started server and created projects GET endpoint

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,22 @@
 // Write your "projects" router here!
+const express = require('express');
+const Projects = require('./projects-model');
+
+const router = express.Router();
+
+router.get('/', async (req, res, next) => {
+    try {
+        const projects = await Projects.get();
+        res.status(200).json(projects);
+    } catch(err) {
+        next(err);
+    }
+});
+
+router.use((err, req, res, next) => { // eslint-disable-line
+    res.status(err.status || 500).json({
+        message: err.message
+    });
+});
+
+module.exports = router;
