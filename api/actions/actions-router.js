97bc2ff8aa09877c2e8045c95e219e5246234227
Bethany Ingraham <BethFIngraham@gmail.com>
2024-09-10 21:11:10 -0400

    Actions router started w GET endpoint

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,24 @@
 // Write your "actions" router here!
+const express = require('express');
+// bring in middleware
+const Actions = require('./actions-model');
+
+const router = express.Router();
+
+router.get('/', async (req, res, next) => {
+    try {
+        const actions = await Actions.get();
+        res.status(200).json(actions);
+    } catch(err) {
+        next(err);
+    }
+});
+
+router.use((err, req, res, next) => { // eslint-disable-line
+    res.status(err.status || 500).json({
+        message: err.message,
+        cutsomMessage: 'Server error'
+    });
+});
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const projectsRouter = require('./projects/projects-router');
+const actionsRouter = require('./actions/actions-router');
 
 const server = express();
 
@@ -11,6 +12,7 @@ const server = express();
 server.use(express.json());
 
 server.use('/api/projects', projectsRouter);
+server.use('/api/actions', actionsRouter);
 
 
 module.exports = server;
