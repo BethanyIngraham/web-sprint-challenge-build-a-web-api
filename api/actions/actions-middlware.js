dfff0bec3be1fb4df1d96a261a99521dbd683f16
Bethany Ingraham <BethFIngraham@gmail.com>
2024-09-11 23:55:19 -0400

    Actions POST endpoint created

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -19,7 +19,34 @@ async function checkActionsId(req, res, next) {
     }
 }
 
+function checkActionsReqBody(req, res, next) {
+    const {project_id, description, notes} = req.body;
+    if(
+        project_id
+        && description !== undefined 
+        && typeof description === 'string'
+        && description.length
+        && description.trim().length
+        && description.length <= 128
+        && notes !== undefined
+        && typeof notes === 'string'
+        && notes.length
+        && notes.trim().length
+    ) {
+        req.project_id = project_id;
+        req.description = description;
+        req.notes = notes;
+        next();
+    } else {
+        next({
+            status: 400,
+            message: 'Please provide project_id, description and notes'
+        });
+    }
+}
+
 
 module.exports = {
     checkActionsId,
+    checkActionsReqBody
 };
--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,7 +1,8 @@
 // Write your "actions" router here!
 const express = require('express');
-const {checkActionsId} = require('./actions-middlware');
+const {checkActionsId, checkActionsReqBody} = require('./actions-middlware');
 const Actions = require('./actions-model');
+const Projects = require('../projects/projects-model');
 
 const router = express.Router();
 
@@ -18,9 +19,28 @@ router.get('/:id', checkActionsId, (req, res) => {
     res.status(200).json(req.action);
 });
 
-// router.post('/', (req, res) => {
-
-// });
+router.post('/', checkActionsReqBody, async (req, res, next) => {
+    try {
+       const actionObject = {
+        project_id: req.body.project_id,
+        description: req.body.description,
+        notes: req.body.notes,
+        completed: req.body.completed ? true : false
+       };
+       const {project_id} = req.body;
+       const project = await Projects.get(project_id);
+       if(!project) {
+        res.status(422).json({
+            message: 'Project id is not valid'
+        });
+       } else {
+         const action = await Actions.insert(actionObject);
+         res.status(201).json(action);
+       }
+    } catch(err) {
+        next(err);
+    }
+});
 
 // router.put('/:id', checkActionsId, (req, res) => {
 
